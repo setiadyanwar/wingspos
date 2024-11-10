95ce4cd9e897b6b7fbf6c7ea5d66259ebe4d50f7
Muhammad Fillah Alfatih <fillahalfatih@users.noreply.github.com>
2024-11-10 23:25:08 +0700

    update routes

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/produk', function () {
+    return view('produk');
+});
+
+Route::get('/checkout', function () {
+    return view('checkout');
 });
