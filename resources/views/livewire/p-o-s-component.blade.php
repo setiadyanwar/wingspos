1c90016b821044e612eff5b6acbe8c22d3b2ecbb
killy147 <ibnuaqil2004@gmail.com>
2024-11-12 18:57:07 +0700

    gambar

--- resources/views/livewire/p-o-s-component.blade.php
+++ resources/views/livewire/p-o-s-component.blade.php
@@ -25,14 +25,20 @@
             <h2 class="text-xl font-bold mb-2 text-black dark:text-white">Keranjang Belanja</h2>
             <ul class="mb-4 text-black dark:text-white">
                 @foreach($cart as $item)
-                    <li class="flex justify-between">
-                        <span>{{ $item['nama_produk'] }}</span>
-                        <span>
-                            <button wire:click="updateCart({{ $item->id }}, false)" class="px-2 bg-red-500 text-white rounded">-</button>
-                            {{ $cartQuantities[$item->id] ?? 0 }}
+                    <li class="flex items-center justify-between mb-2">
+                        <!-- Product Image (Smaller) -->
+                        <img src="{{ asset('storage/' . $item['gambar_produk']) }}" alt="Product Image" class="w-10 h-10 rounded-lg object-cover mr-2" />
+                        <!-- Product Details (Smaller Text) -->
+                        <div class="flex flex-col flex-grow text-sm">
+                            <span class="font-semibold">{{ $item['nama_produk'] }}</span>
+                            <span>Rp {{ number_format($item['harga_produk'], 0, ',', '.') }}</span>
+                        </div>
+                        <!-- Quantity Controls with Yellow and Green Buttons -->
+                        <div class="flex items-center">
+                            <button wire:click="updateCart({{ $item->id }}, false)" class="px-2 bg-yellow-500 text-white rounded">-</button>
+                            <span class="px-2 text-sm">{{ $cartQuantities[$item->id] ?? 0 }}</span>
                             <button wire:click="updateCart({{ $item->id }}, true)" class="px-2 bg-green-500 text-white rounded">+</button>
-                        </span>
-                        <span>Rp {{ number_format($item['harga_produk'] * $cartQuantities[$item->id], 0, ',', '.') }}</span>
+                        </div>
                     </li>
                 @endforeach
             </ul>
