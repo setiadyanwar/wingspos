e299e8d58bc208873c7998684fbfe7bd28ad5ef5
setiadyanwar <setiadyanwar@gmail.com>
2024-11-13 11:22:17 +0700

    hide POS

--- app/Filament/Pages/POSPage.php
+++ app/Filament/Pages/POSPage.php
@@ -6,91 +6,91 @@ use Filament\Pages\Page;
 use App\Models\POS;
 use App\Models\Order;
 
-class POSPage extends Page
-{
-    protected static ?string $navigationLabel = 'POS';
-    protected static ?string $navigationIcon = 'heroicon-o-document-currency-rupee';
-    protected static ?string $slug = 'pos';
-    protected static string $view = 'filament.pages.pos-page';
+// class POSPage extends Page
+// {
+//     protected static ?string $navigationLabel = 'POS';
+//     protected static ?string $navigationIcon = 'heroicon-o-document-currency-rupee';
+//     protected static ?string $slug = 'pos';
+//     protected static string $view = 'filament.pages.pos-page';
 
-    public $products;         // Menyimpan data produk
-    public $cart = [];        // Menyimpan produk yang dipilih
-    public $cartQuantities = []; // Menyimpan jumlah produk per ID
-    public $totalPrice = 0;
-    public $totalItems = 0;
+//     public $products;         // Menyimpan data produk
+//     public $cart = [];        // Menyimpan produk yang dipilih
+//     public $cartQuantities = []; // Menyimpan jumlah produk per ID
+//     public $totalPrice = 0;
+//     public $totalItems = 0;
 
-    public function mount()
-    {
-        // Mengambil data produk dari database
-        $this->products = POS::all(); // Sesuaikan query sesuai kebutuhan Anda
-    }
+//     public function mount()
+//     {
+//         // Mengambil data produk dari database
+//         $this->products = POS::all(); // Sesuaikan query sesuai kebutuhan Anda
+//     }
 
-    // Menambahkan produk ke keranjang
-    public function addToCart($productId)
-    {
-        $product = POS::find($productId);
+//     // Menambahkan produk ke keranjang
+//     public function addToCart($productId)
+//     {
+//         $product = POS::find($productId);
 
-        if ($product) {
-            $this->cart[$productId] = $product;
-            $this->cartQuantities[$productId] = ($this->cartQuantities[$productId] ?? 0) + 1;
-            $this->calculateTotals();
-        }
-    }
+//         if ($product) {
+//             $this->cart[$productId] = $product;
+//             $this->cartQuantities[$productId] = ($this->cartQuantities[$productId] ?? 0) + 1;
+//             $this->calculateTotals();
+//         }
+//     }
 
-    // Memperbarui jumlah produk dalam keranjang
-    public function updateCart($productId, $increase)
-    {
-        if (isset($this->cartQuantities[$productId])) {
-            if ($increase) {
-                $this->cartQuantities[$productId]++;
-            } else {
-                $this->cartQuantities[$productId] = max(0, $this->cartQuantities[$productId] - 1);
-                if ($this->cartQuantities[$productId] === 0) {
-                    unset($this->cart[$productId]); // Hapus produk dari keranjang jika jumlahnya 0
-                }
-            }
-            $this->calculateTotals();
-        }
-    }
+//     // Memperbarui jumlah produk dalam keranjang
+//     public function updateCart($productId, $increase)
+//     {
+//         if (isset($this->cartQuantities[$productId])) {
+//             if ($increase) {
+//                 $this->cartQuantities[$productId]++;
+//             } else {
+//                 $this->cartQuantities[$productId] = max(0, $this->cartQuantities[$productId] - 1);
+//                 if ($this->cartQuantities[$productId] === 0) {
+//                     unset($this->cart[$productId]); // Hapus produk dari keranjang jika jumlahnya 0
+//                 }
+//             }
+//             $this->calculateTotals();
+//         }
+//     }
 
-    // Menghitung total harga dan total item
-    public function calculateTotals()
-    {
-        $this->totalPrice = 0;
-        $this->totalItems = 0;
+//     // Menghitung total harga dan total item
+//     public function calculateTotals()
+//     {
+//         $this->totalPrice = 0;
+//         $this->totalItems = 0;
     
-        foreach ($this->cart as $productId => $item) {
-            $quantity = $this->cartQuantities[$productId] ?? 0;
-            $this->totalPrice += $item['harga_produk'] * $quantity;
-            $this->totalItems += $quantity;
-        }
-        dd($this->totalPrice, $this->totalItems);
-    }
+//         foreach ($this->cart as $productId => $item) {
+//             $quantity = $this->cartQuantities[$productId] ?? 0;
+//             $this->totalPrice += $item['harga_produk'] * $quantity;
+//             $this->totalItems += $quantity;
+//         }
+//         dd($this->totalPrice, $this->totalItems);
+//     }
     
 
-    // Fungsi untuk checkout dan menyimpan pesanan ke database
-    public function submitOrder()
-    {
-        $this->calculateTotals(); // Pastikan nilai total diperbarui sebelum penyimpanan
+//     // Fungsi untuk checkout dan menyimpan pesanan ke database
+//     public function submitOrder()
+//     {
+//         $this->calculateTotals(); // Pastikan nilai total diperbarui sebelum penyimpanan
 
-        Order::create([
-            'total_price' => $this->totalPrice,
-            'total_items' => $this->totalItems,
-            // tambahkan field lain jika perlu
-        ]);
+//         Order::create([
+//             'total_price' => $this->totalPrice,
+//             'total_items' => $this->totalItems,
+//             // tambahkan field lain jika perlu
+//         ]);
 
-        // Reset keranjang setelah checkout
-        $this->resetCart();
-    }
+//         // Reset keranjang setelah checkout
+//         $this->resetCart();
+//     }
 
 
-    // Mengosongkan keranjang setelah checkout
-    public function resetCart()
-    {
-        $this->cart = [];
-        $this->cartQuantities = [];
-        $this->totalPrice = 0;
-        $this->totalItems = 0;
-    }
+//     // Mengosongkan keranjang setelah checkout
+//     public function resetCart()
+//     {
+//         $this->cart = [];
+//         $this->cartQuantities = [];
+//         $this->totalPrice = 0;
+//         $this->totalItems = 0;
+//     }
 
-}
+// }
